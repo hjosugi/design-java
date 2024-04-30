6a6b1e16a398d5e134c67b8f1507d0e08b1ab016
hjosugi <h.jo.sugi@gmail.com>
2024-04-30 13:44:11 +0000

    add command pattern

---
+++ src/main/java/command/MacroCommand.java
@@ -0,0 +1,32 @@
+package command;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MacroCommand implements Command {
+  private Deque<Command> commands = new ArrayDeque<>();
+
+  @Override
+  public void execute() {
+    for (Command command : commands) {
+      command.execute();
+    }
+  }
+
+  public void append(Command command) {
+    if (command == this) {
+      throw new IllegalArgumentException("Infinite loop by append");
+    }
+    commands.push(command);
+  }
+
+  public void undo() {
+    if (!commands.isEmpty()) {
+      commands.pop();
+    }
+  }
+
+  public void clear() {
+    commands.clear();
+  }
+}
